a7d49d99ff01f24bdcaf49dcb906970e506803b6
IcaroL-byte <icaro.lauan@escolar.ifrn.edu.br>
2026-05-04 20:08:00 -0300

    CenaSplash

---
+++ initialProject/Assets/SourceFiles/Scripts/SplashController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(Splash());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Splash()
+    {
+        yield return new WaitForSeconds(2f);
+        if (GameMenager.Instance != null)
+        {
+            GameMenager.Instance.CarregarCena("MenuPrincipal");
+        }
+    }
+}
